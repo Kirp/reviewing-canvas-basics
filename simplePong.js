94f029589d140527534a4968adaa5f79fec80670
Kierro DeCampanille <pentawick@gmail.com>
2022-02-19 20:49:12 +0800

    end of tutorial study

--- simplePong.js
+++ simplePong.js
@@ -15,6 +15,58 @@ var paddleX = (canvas.width-paddleWidth)/2;
 var rightPressed = false;
 var leftPressed = false;
 
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+
+var score = 0;
+
+var lives = 3;
+
+var bricks = [];
+for(var c=0; c<brickColumnCount; c++){
+    bricks[c] = [];
+    for(var r=0; r<brickRowCount; r++){
+        bricks[c][r]={x:0, y:0, status:1};
+
+    }
+}
+
+function drawLives(){
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Lives: "+lives, canvas.width-65, 20);
+}
+
+function drawScore() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Score: "+score, 8, 20);
+}
+
+function drawBricks(){
+    for(var c=0; c<brickColumnCount; c++){
+        for(var r=0; r<brickRowCount; r++){
+            if(bricks[c][r].status == 1){
+                var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+                var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX,brickY,brickWidth, brickHeight);
+                ctx.fillStyle = "#0095DD";
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+}
+
 function drawBall(){
     ctx.beginPath();
     ctx.arc(x,y,ballRadius,0,Math.PI*2);
@@ -34,6 +86,10 @@ function drawPaddle() {
 function draw(){
     //drawing code
     ctx.clearRect(0,0, canvas.width, canvas.height);
+
+
+    drawBricks();
+
     drawBall();
 
     if(rightPressed){
@@ -51,6 +107,9 @@ function draw(){
     }
 
     drawPaddle();
+    drawScore();
+    drawLives();
+    collisionDetection();
     if(x+dx > canvas.width -ballRadius|| x+dx <ballRadius){
         dx = -dx;
     }
@@ -62,18 +121,41 @@ function draw(){
             dy = -dy;
         }else {
 
-            //alert("Game Over!");
-            console.log("GAME OVER");
-            clearInterval(interval);
-            //document.location.reload();
+            lives--;
+            if(!lives){
+
+            alert("Game Over!");
+            //console.log("GAME OVER");
+            //clearInterval(interval);
+            document.location.reload();
+            }else {
+                x = canvas.width/2;
+                y = canvas.height -30;
+                dx = 2;
+                dy = -2;
+                paddleX = (canvas.width-paddleWidth)/2;
+            }
+
+
+
         }
     }
     x+=dx;
     y+=dy;
+
+    requestAnimationFrame(draw);
 }
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("mousemove", mouseMoveHandler, false);
+
+function mouseMoveHandler(e){
+    var relativeX = e.clientX - canvas.offsetLeft;
+    if(relativeX > 0 && relativeX < canvas.width){
+        paddleX = relativeX - paddleWidth/2;
+    }
+}
 
 function keyDownHandler(e){
     if(e.key == "Right" || e.key == "ArrowRight"){
@@ -91,4 +173,32 @@ function keyUpHandler(e){
     }
 }
 
-var interval = setInterval(draw, 10);
+
+
+
+function collisionDetection(){
+    for(var c=0; c<brickColumnCount; c++){
+        for(var r=0; r<brickRowCount; r++){
+            var b = bricks[c][r];            
+            //calculations
+            if(b.status ==1){
+                if( x>b.x && x <b.x+brickWidth && y> b.y && y <b.y+brickHeight)
+                {
+                    dy=-dy;
+                    b.status = 0;
+                    score++;
+                    if(score >= brickRowCount*brickColumnCount)
+                    {
+                        alert("YOU WIN, CONGRATULATIONS!");
+                        document.location.reload();
+                        //clearInterval(interval);
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+
+draw();
